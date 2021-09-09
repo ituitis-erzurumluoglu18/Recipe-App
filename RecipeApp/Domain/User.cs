c29c0d58a73b939521937d0b82f5a9cfe003da3c
ituitis-erzurumluoglu18 <erzurumluoglu18@itu.edu.tr>
2021-09-09 17:40:10 +0300

    Nhibernate implementation

---
+++ RecipeApp/Domain/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeApp.Domain
+{
+    public class User
+    {
+        //[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PhotoUrl { get; set; }
+    }
+}
